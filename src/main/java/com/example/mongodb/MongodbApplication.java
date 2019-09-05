899cc4e9fbd06ba36f59f085889986b46b522e47
skrudra <skrudra12@gmail.com>
2019-09-05 21:05:13 +0530

    Restructure

--- src/main/java/com/example/mongodb/MongodbApplication.java
+++ src/main/java/com/example/mongodb/MongodbApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.mongodb.beans.Customer;
-import com.example.mongodb.beans.CustomerRepository;
+import com.example.mongodb.dao.CustomerRepository;
 
 @SpringBootApplication
 public class MongodbApplication implements CommandLineRunner {
--- src/main/java/com/example/mongodb/dao/CustomerRepository.java
+++ src/main/java/com/example/mongodb/dao/CustomerRepository.java
@@ -1,10 +1,12 @@
-package com.example.mongodb.beans;
+package com.example.mongodb.dao;
 
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.example.mongodb.beans.Customer;
 
 @RepositoryRestResource(collectionResourceRel = "people", path = "people")
 public interface CustomerRepository extends MongoRepository<Customer, String> {
